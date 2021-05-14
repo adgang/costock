23a1555b6161dfaf1329fe1668b970d662340bdd
Aditya Gangipamula <adgang@tuta.io>
2021-05-15 00:18:03 +0530

    added links to donation and request

--- costock-react/src/App.js
+++ costock-react/src/App.js
@@ -1,21 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import 'antd/dist/antd.css';
-import { Button } from 'antd';
+import logo from "./logo.svg";
+import "./App.css";
+import "antd/dist/antd.css";
+import { Button } from "antd";
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={'o2-rect.png'} className="App-logo" alt="logo" />
+        <img src={"o2-rect.png"} className="App-logo" alt="logo" />
         <div class="App-title">
-          <h1>O<sub>2</sub> Concentrator Stockpile</h1>
+          <h1>
+            O<sub>2</sub> Concentrator Stockpile
+          </h1>
         </div>
       </header>
       <section>
-      <Button type="primary">Button</Button>
-
+        <Router>
+          <Link to="/donation"><Button>I want to donate</Button></Link>
+          <Link to="/requests"><Button>I want to request a concentrator</Button></Link>
+        </Router>
       </section>
       <footer></footer>
     </div>
