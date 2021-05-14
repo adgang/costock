eafb842aedb5bec75b8c6449e5077dab665fab04
Aditya Gangipamula <adgang@tuta.io>
2021-05-14 23:58:44 +0530

    added rect logo

--- costock-react/src/App.js
+++ costock-react/src/App.js
@@ -5,11 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={'o2'} className="App-logo" alt="logo" />
+        <img src={'o2-rect.png'} className="App-logo" alt="logo" />
         <div class="App-title">
           <h1>O<sub>2</sub> Concentrator Stockpile</h1>
         </div>
       </header>
+      <section>
+
+      </section>
+      <footer></footer>
     </div>
   );
 }
