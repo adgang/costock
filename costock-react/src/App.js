c1a5118d345a2b902d0d1a59f0911e6a13149c5b
Aditya Gangipamula <adgang@tuta.io>
2021-05-14 23:48:30 +0530

    customized UI for app

--- costock-react/src/App.js
+++ costock-react/src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={'o2'} className="App-logo" alt="logo" />
+        <div class="App-title">
+          <h1>O<sub>2</sub> Concentrator Stockpile</h1>
+        </div>
       </header>
     </div>
   );
