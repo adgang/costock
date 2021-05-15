c516fe0627ffb084d6599160a847f373383ea307
Aditya Gangipamula <adgang@tuta.io>
2021-05-15 21:08:58 +0530

    added tests for open api axios client

---
+++ costock-react/test/openapi.js
@@ -0,0 +1,16 @@
+const { OpenAPIClientAxios } = require("openapi-client-axios");
+
+const api = new OpenAPIClientAxios({
+  definition: "http://localhost:3200/swagger.yaml",
+});
+api.init();
+
+describe("APIs", function () {
+  describe("#donations", function () {
+    it("should create a donation", async function () {
+      const client = await api.getClient();
+      
+      console.log(client);
+    });
+  });
+});
