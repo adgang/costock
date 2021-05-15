f02c58499ee03d05cd45c2e5010ea0079942f4d6
Aditya Gangipamula <adgang@tuta.io>
2021-05-15 06:12:42 +0530

    swagger validators added

--- index.js
+++ index.js
@@ -1,6 +1,55 @@
 const express = require("express");
+const OpenApiValidator = require('express-openapi-validator')
+
+const {connector, summarise} = require('swagger-routes-express')
+const YAML = require('yamljs')
 const pathToSwaggerUi = require("swagger-ui-dist").absolutePath();
+
+
+const yamlSpecFile = './public/swagger.yaml'
+const apiDefinition = YAML.load(yamlSpecFile)
+const apiSummary = summarise(apiDefinition)
+console.info(apiSummary)
+
 const app = express();
+
 app.use("/swagger-ui", express.static(pathToSwaggerUi));
 app.use(express.static('public'))
+
+const validatorOptions = {
+  coerceTypes: true,
+  apiSpec: yamlSpecFile,
+  validateRequests: true,
+  validateResponses: true
+}
+app.use(OpenApiValidator.middleware(validatorOptions))
+  
+// error customization, if request is invalid
+app.use((err, req, res, next) => {
+  res.status(err.status).json({
+    error: {
+      type: 'request_validation',
+      message: err.message,
+      errors: err.errors
+    }
+  })
+})
+
+const api = {
+  listModels: (req, res) => {
+    console.log(req)
+    console.log("bb")
+    res.json({"a": 1})
+  }
+}
+
+const connect = connector(api, apiDefinition, {
+  onCreateRoute: (method, descriptor) => {
+    console.log(`${method}: ${descriptor[0]} : ${(descriptor[1]).name}`)
+  }
+})
+
+connect(app)
+
+
 app.listen(3200);
