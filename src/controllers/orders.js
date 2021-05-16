ae47664d42d2f3fbb2a16011a4d5bfc0e61a9b45
Aditya Gangipamula <adgang@tuta.io>
2021-05-16 18:54:29 +0530

    fixed order schema

--- src/controllers/orders.js
+++ src/controllers/orders.js
@@ -1,18 +1,43 @@
+const {
+  DEVICE_PREFIX,
+  UNKNOWN_ERROR_RESPONSE,
+  DEVICE_BY_LOCATION_INDEX,
+  DEVICE_BY_MODEL_PREFIX,
+  AVAILABLE_DEVICE_BY_MODEL_PREFIX,
+  AVAILABLE_DEVICES_BY_LOCATION_INDEX,
+  FEASIBLE_DELIVERY_DISTANCE_KM,
+} = require("../constants");
+const { v4: uuidv4 } = require("uuid");
+const { redisKey } = require("../utils");
+
+const debug = require("debug")("controllers:orders");
+
 function orderController(redis) {
-  function listOrders(req, res, next) {
+  async function listOrders(req, res, next) {
     console.log("list Orders not implemented");
     res.json([]);
   }
 
-  function addOrder(req, res, next) {
-    console.log("add Order not implemented");
+  async function addOrder(req, res, next) {
+    debug("request body:", req.body);
+    const uuid = uuidv4();
+    const time = new Date().getTime();
+
+    const order = {
+      ...req.body,
+      id: uuid,
+      created_at: time,
+      status: "waitlisted",
+    };
+    debug("response body:", order);
+    res.json(order);
   }
 
-  function getOrder(req, res, next) {
-    console.log("get Order not implemented");
+  async function getOrder(req, res, next) {
+    debug("request params:", req.params);
   }
 
-  function editOrder(req, res, next) {
+  async function editOrder(req, res, next) {
     console.log("edit Order not implemented");
   }
 
